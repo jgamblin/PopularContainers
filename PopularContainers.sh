2f6be0def7b5b2b7605c1cc7c0a4db76e7528faf
Jerry Gamblin <jerry.gamblin@gmail.com>
2019-05-12 17:25:57 -0500

    Fixing Typo
    Fixing A Typo.

--- PopularContainers.sh
+++ PopularContainers.sh
@@ -4,6 +4,6 @@ page=0
 #Pull 10 Pages Of Containers With 100 Per Page
 while [ $page -lt 10 ]
 do
-curl -s "https://store.docker.com/api/content/v1/products/search?page_size=100&page=${page}&q=%2B&source=community&type=image%2Cbundle" | jq -r '.summaries | sort_by(-.popularity) | .[].name' >> MostPopularConatiners-"$(date +"%m_%d_%Y")".txt
+curl -s "https://store.docker.com/api/content/v1/products/search?page_size=100&page=${page}&q=%2B&source=community&type=image%2Cbundle" | jq -r '.summaries | sort_by(-.popularity) | .[].name' >> MostPopularContainers-"$(date +"%m_%d_%Y")".txt
 page=$((page + 1))
 done
